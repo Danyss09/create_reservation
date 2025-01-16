9a49ecbbd1dfb18e5e235c7ef22ca5dac940caf2
Danyss09 <danielacaceresp4@gmail.com>
2025-01-16 15:39:14 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 import asyncpg
-from fastapi import FastAPI
 
-DATABASE_URL = "postgresql://username:password@localhost/ReservationDB"
+DATABASE_URL = "postgresql://postgres:dani0919@localhost/ReservationCreateDB"
 
 async def get_db_connection():
     return await asyncpg.connect(DATABASE_URL)
